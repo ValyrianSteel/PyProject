2747745b7784d7efa2a3ac27002d1f8fe6dcb0cd
ValyrianSteel <chengmingma@foxmail.com>
2017-11-17 13:47:27 +0800

    个税计算器

---
+++ calculator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+
+def calc(wage):
+    tax_get = wage - 3500
+    
+    if tax_get <= 0:
+        return 0
+    elif 0 < tax_get <= 1500:
+        return tax_get*0.03 - 0
+    elif 1500 < tax_get <= 4500:
+        return tax_get*0.1 - 105
+    elif 4500 < tax_get <= 9000:
+        return tax_get*0.2 - 555
+    elif 9000 < tax_get <= 35000:
+        return tax_get*0.25 - 1005
+    elif 35000 < tax_get <= 55000:
+        return tax_get*0.3 - 2755
+    elif 55000 < tax_get <= 80000:
+        return tax_get*0.35 - 5505
+    elif 80000 < tax_get:
+        return tax_get*0.45 - 13505
+
+
+if __name__ == "__main__":
+    wage = 0
+    try:
+        wage = int(sys.argv[1])
+    except:
+        print("Parameter Error")
+
+    tax = calc(wage)
+
+    print(format(tax, ".2f"))
